c1ed089cfb40ab7d6c14644976c767d5900fb720
yasmineRajhi <yasminerajhi2000@gmail.com>
2021-10-11 20:32:28 +0100

    entry 2

---
+++ ExchangeRate.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+let date = document.querySelector("input[type=date]").value;
+console.log(date);
+const url = `http://api.exchangeratesapi.io/v1/${date}?access_key=876fddb6aa3434033523b5285d1992f5&format=1`
+let rates
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+})
+fetch(url).then(response => response.json()).then(data => {
+    rates = data.rates;
+}).catch(error => {
+    console.log("error :",error)
+})
+
+document.querySelector("button").onclick = () => {
+    let value = document.querySelector("input[type=text]").value;
+    let exchangeRate = document.querySelector("select").value;
+    let totalExchange = value * rates[exchangeRate]
+    document.querySelector(".result").innerText = `${value} in EUR is ${totalExchange} en ${exchangeRate}`
+}
+
+function dateUpdate(){
+    let dateUpdate = document.querySelector("input[type=date]").value;
+    date = dateUpdate;
+    console.log(date);
+}
