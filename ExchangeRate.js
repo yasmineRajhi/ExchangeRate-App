f49dc5dbbfbe3dc08dc64c0d208f4c2893396611
yasmineRajhi <yasminerajhi2000@gmail.com>
2021-10-13 18:29:10 +0100

    jawha behi kolha ti5dem

--- ExchangeRate.js
+++ ExchangeRate.js
@@ -1,7 +1,6 @@
 const form = document.querySelector("form");
-let date = document.querySelector("input[type=date]").value;
-console.log(date);
-let url = `http://api.exchangeratesapi.io/v1/${date}?access_key=876fddb6aa3434033523b5285d1992f5&format=1`;
+let dateInput = document.querySelector("input[type=date]");
+let url = `http://api.exchangeratesapi.io/v1/latest?access_key=876fddb6aa3434033523b5285d1992f5&format=1`;
 //dateUpdate(url);
 let rates
 form.addEventListener("submit", (e) =>{
@@ -9,20 +8,28 @@ form.addEventListener("submit", (e) =>{
 })
 fetch(url).then(response => response.json()).then(data => {
     rates = data.rates;
+    console.log(rates)
 }).catch(error => {
     console.log("error :",error)
 })
 
 document.querySelector("button").onclick = () => {
     let value = document.querySelector("input[type=text]").value;
+    
     let exchangeRate = document.querySelector("select").value;
+    console.log(value,exchangeRate);
     let totalExchange = value * rates[exchangeRate]
     document.querySelector(".result").innerText = `${value} in EUR is ${totalExchange} en ${exchangeRate}`
 }
 
 function dateUpdate(){
-    let dateUpdate = document.querySelector("input[type=date]").value;
-    date = dateUpdate;
-    url = `http://api.exchangeratesapi.io/v1/${date}?access_key=876fddb6aa3434033523b5285d1992f5&format=1`
-    console.log(date);
+    let dateUpdate = dateInput.value;
+    url = `http://api.exchangeratesapi.io/v1/${dateUpdate}?access_key=876fddb6aa3434033523b5285d1992f5&format=1`
+    console.log(dateUpdate);
+    fetch(url).then(response => response.json()).then(data => {
+    rates = data.rates;
+    console.log(rates)
+}).catch(error => {
+    console.log("error :",error)
+})
 }
